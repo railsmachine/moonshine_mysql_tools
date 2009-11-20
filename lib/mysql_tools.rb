ae2a36d176d3ec188b6587418beaf866a212c23e
Jesse Newland <jnewland@gmail.com>
2009-11-19 17:01:00 -0800

    initial commit

---
+++ lib/mysql_tools.rb
@@ -0,0 +1,53 @@
+module MysqlTools
+
+  def mysql_tools(hash = {})
+    options = {
+      :maatkit => "http://maatkit.googlecode.com/files/maatkit_5014-1_all.deb",
+      :xtrabackup => "http://www.percona.com/mysql/xtrabackup/0.9/deb/xtrabackup_0.9_amd64.deb",
+    }.merge(hash)
+
+    package 'mytop', :ensure => :installed
+    package 'wget', :ensure => :installed
+    file '/usr/local/src',
+      :ensure => :directory # same here
+
+    exec 'wget mysqltuner.pl',
+      :cwd => "/usr/sbin/",
+      :creates => "/usr/sbin/mysqltuner.pl",
+      :require => package('wget')
+
+    if options[:maatkit]
+
+      maatkit_deb = options[:maatkit].split('/').last
+
+      exec 'wget maatkit',
+        :command => "wget #{options[:maatkit]}",
+        :cwd => "/usr/local/src",
+        :creates => "/usr/local/src/#{maatkit_deb}",
+        :require => package('wget')
+
+      package "maatkit",
+         :ensure => "installed",
+         :provider => "dpkg",
+         :source => "/usr/local/src/#{maatkit_deb}",
+         :require => exec('wget maatkit')
+    end
+
+    if options[:xtrabackup]
+
+      xtrabackup_deb = options[:xtrabackup].split('/').last
+
+      exec 'wget xtrabackup',
+        :command => "wget #{options[:xtrabackup]}",
+        :cwd => "/usr/local/src",
+        :creates => "/usr/local/src/#{xtrabackup_deb}",
+        :require => package('wget')
+      package "xtrabackup",
+         :ensure => "installed",
+         :provider => "dpkg",
+         :source => "/usr/local/src/#{xtrabackup_deb}",
+         :require => exec('wget xtrabackup')
+    end
+  end
+  
+end
---
+++ spec/mysql_tools_spec.rb
@@ -0,0 +1,29 @@
+require File.join(File.dirname(__FILE__), 'spec_helper.rb')
+
+class MysqlToolsManifest < Moonshine::Manifest
+  plugin :mysql_tools
+end
+
+describe "A manifest with the MysqlTools plugin" do
+
+  before do
+    @manifest = MysqlToolsManifest.new
+  end
+
+  it "should be executable" do
+    @manifest.should be_executable
+  end
+
+  it "should provide common mysql tools" do
+    @manifest.mysql_tools
+    @manifest.execs['wget xtrabackup'].should_not be_nil
+    @manifest.execs['wget maatkit'].should_not be_nil
+    @manifest.execs['wget mysqltuner.pl'].should_not be_nil
+    @manifest.packages['mytop'].should_not be_nil
+  end
+
+  it "can disable xtrabackup" do
+    @manifest.mysql_tools :xtrabackup => nil
+    @manifest.execs['wget xtrabackup'].should be_nil
+  end
+end
